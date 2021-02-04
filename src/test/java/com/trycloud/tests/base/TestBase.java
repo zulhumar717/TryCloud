5246e54bd290500891294aab0d580e0b40d809a9
Kiarra Kelly <keke5599@gmail.com>
2021-02-04 11:40:58 -0500

    testbase update

--- src/test/java/com/trycloud/tests/base/TestBase.java
+++ src/test/java/com/trycloud/tests/base/TestBase.java
@@ -3,14 +3,16 @@ package com.trycloud.tests.base;
 import com.trycloud.utilities.ConfigurationReader;
 import com.trycloud.utilities.Driver;
 import com.trycloud.utilities.WebDriverFactory;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 import java.util.concurrent.TimeUnit;
 
-public class TestBase {
+public abstract class TestBase {
 
     public WebDriver driver;
 
@@ -23,13 +25,25 @@ public class TestBase {
     }
 
     @Test
-    public void testName() {
+    public void verify_user_login() {
+
+        driver.get(ConfigurationReader.getProperty("url"));
+
+        WebElement usernameLogin = driver.findElement(By.id("user"));
+        usernameLogin.sendKeys("User39");
+
+        WebElement passwordLogin = driver.findElement(By.id("password"));
+        passwordLogin.sendKeys("Userpass123");
+
+        WebElement loginButton = driver.findElement(By.xpath("//input[@id='submit']"));
+        loginButton.click();
+
 
     }
 
     @AfterMethod
     public void tearDown(){
-        driver.close();
+      //  driver.close();
     }
 
 }
